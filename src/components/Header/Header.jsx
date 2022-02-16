2736d9840c6485767ca5d20cd7e01d00f15fc5ce
Zamdikhanov <93732183+Zamdikhanov@users.noreply.github.com>
2022-02-16 23:44:03 +0400

    add header, variables, mixins

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { ReactComponent as SvgMapPosition } from '../../assets/icons/map-postion.svg';
+import css from './Header.module.scss';
+
+const Header = () => {
+    return (
+        <menu className={css.header}>
+            <NavLink className={css.header__logo} to='#' >
+                <h3 className={css.logo__title}>
+                    Need for drive
+                </h3>
+            </NavLink>
+            <NavLink className={css.header__location} to='#'>
+                <SvgMapPosition className={css.location__svg} />
+                <h3 className={css.location__name}>
+                    Ульяновск
+                </h3>
+            </NavLink>
+        </menu>
+    )
+}
+
+export default Header;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,6 @@
-import react from "react";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Header from "../../components/Header/Header";
 import Sidebar from "../../components/SideBar/Sidebar";
 import css from './MainPage.module.scss';
 
@@ -11,10 +13,21 @@ const MainPage = () => {
             <main className={css.mainContainer__content}>
                 <div className={css.content__mainBlock}>
                     <div className={css.mainBlock_header}>
-                        
+                        <Header />
                     </div>
                     <div className={css.mainBlock_heroBlock}>
-                    Каршеринг
+                        <h2 className={css.heroBlock__caption}>
+                            Каршеринг
+                        </h2>
+                        <h2 className={css.heroBlock__title}>
+                            Need for drive
+                        </h2>
+                        <div className={css.heroBlock__descrption}>
+                            Поминутная аренда авто твоего города
+                        </div>
+                        <NavLink className={css.heroBlock__button} to='#'>
+                            Забронировать
+                        </NavLink>
                     </div>
                     <div className={css.mainBlock_footer}>
 

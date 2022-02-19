d48f09d3ad2701b2b060355399889b835dfa4f45
Zamdikhanov <93732183+Zamdikhanov@users.noreply.github.com>
2022-02-19 20:43:28 +0400

    add slider - swiper

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+// import Swiper, { Navigation, Pagination } from 'swiper';
+import  {Swiper,  Autoplay, EffectFade, Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './Slider.scss';
 import Slide from './Slide/Slide';
 import imgSlide1 from './../../assets/images/slider/slide-1.jpg';
 import imgSlide2 from './../../assets/images/slider/slide-2.jpg';
 import imgSlide3 from './../../assets/images/slider/slide-3.jpg';
 import imgSlide4 from './../../assets/images/slider/slide-4.jpg';
+import { ReactComponent as Prev } from '../../assets/icons/prev.svg';
+import { ReactComponent as Next } from '../../assets/icons/next.svg';
+
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+      },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
+
+Swiper.use([Autoplay, EffectFade, Navigation, Pagination]);
 
 const slidesData = [
     {
@@ -38,9 +61,18 @@ const slidesData = [
 
 const Slider = () => {
     return (
-        <>
-            <Slide {...slidesData[0]} />
-        </>
+        <div className='swiper'>
+            <div className='swiper-wrapper'>
+                <div className="swiper-slide"><Slide {...slidesData[0]}/></div>
+                <div className="swiper-slide"><Slide {...slidesData[1]}/></div>
+                <div className="swiper-slide"><Slide {...slidesData[2]}/></div>
+                <div className="swiper-slide"><Slide {...slidesData[3]}/></div>
+            </div>
+            <div className="swiper-pagination"></div>
+            <div className="swiper-button-prev"><Prev /></div>
+            <div className="swiper-button-next"><Next /></div>
+            <div className="swiper-scrollbar"></div>
+        </div>
     )
 }
 

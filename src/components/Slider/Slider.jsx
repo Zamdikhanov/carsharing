2bd5b7e25777e795bec17b834e660ace5f4e87a6
Zamdikhanov <oxothuk.sky@mail.ru>
2022-02-20 08:38:09 +0400

    continue on another computer

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-// import Swiper, { Navigation, Pagination } from 'swiper';
-import  {Swiper,  Autoplay, EffectFade, Navigation, Pagination } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
 import './Slider.scss';
 import Slide from './Slide/Slide';
 import imgSlide1 from './../../assets/images/slider/slide-1.jpg';
@@ -12,21 +12,6 @@ import imgSlide3 from './../../assets/images/slider/slide-3.jpg';
 import imgSlide4 from './../../assets/images/slider/slide-4.jpg';
 import { ReactComponent as Prev } from '../../assets/icons/prev.svg';
 import { ReactComponent as Next } from '../../assets/icons/next.svg';
-
-const swiper = new Swiper('.swiper', {
-    // configure Swiper to use modules
-    direction: 'horizontal',
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-      },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-});
-
-Swiper.use([Autoplay, EffectFade, Navigation, Pagination]);
 
 const slidesData = [
     {
@@ -61,18 +46,20 @@ const slidesData = [
 
 const Slider = () => {
     return (
-        <div className='swiper'>
-            <div className='swiper-wrapper'>
-                <div className="swiper-slide"><Slide {...slidesData[0]}/></div>
-                <div className="swiper-slide"><Slide {...slidesData[1]}/></div>
-                <div className="swiper-slide"><Slide {...slidesData[2]}/></div>
-                <div className="swiper-slide"><Slide {...slidesData[3]}/></div>
-            </div>
-            <div className="swiper-pagination"></div>
-            <div className="swiper-button-prev"><Prev /></div>
-            <div className="swiper-button-next"><Next /></div>
-            <div className="swiper-scrollbar"></div>
-        </div>
+        <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={0}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+        >
+            {slidesData.map(slide => {
+                return (
+                    <SwiperSlide key={slide.id}><Slide key={slide.id} {...slide} /></SwiperSlide>
+                )
+            })}
+        </Swiper>
     )
 }
 

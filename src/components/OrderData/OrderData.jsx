9ab910bcf0540de135cc45015b26ed4c305dba4b
Zamdikhanov <oxothuk.sky@mail.ru>
2022-02-28 23:21:33 +0400

    finished adaptive

--- src/components/OrderData/OrderData.jsx
+++ src/components/OrderData/OrderData.jsx
@@ -4,21 +4,23 @@ import css from './OrderData.module.scss';
 function OrderData() {
     return (
         <div className={css.container}>
-            <div className={css.title}>
-                <h4 className={css.titleText}>Ваш заказ:</h4>
+            <div className={css.wrap} >
+                <div className={css.title}>
+                    <h4 className={css.titleText}>Ваш заказ:</h4>
+                </div>
+                <ul className={css.dataList}>
+                    <li className={css.dataList__item}>
+                        <div className={css.item__name}>Пункт выдачи</div>
+                        <div className={css.item__dots}>
+                            <span />
+                        </div>
+                        <div className={css.item__value}>
+                            Ульяновск, Нариманова 42
+                        </div>
+                    </li>
+                </ul>
+                <div className={css.price}>Цена: от 8 000 до 12 000 ₽</div>
             </div>
-            <ul className={css.dataList}>
-                <li className={css.dataList__item}>
-                    <div className={css.item__name}>Пункт выдачи</div>
-                    <div className={css.item__dots}>
-                        <span />
-                    </div>
-                    <div className={css.item__value}>
-                        Ульяновск, Нариманова 42
-                    </div>
-                </li>
-            </ul>
-            <div className={css.price}>Цена: от 8 000 до 12 000 ₽</div>
             <NavLink className={css.button} to="#">
                 Выбрать модель
             </NavLink>

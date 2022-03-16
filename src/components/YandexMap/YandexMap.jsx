308f8afde10f8454ec9e8a11b0b84a95de441062
Zamdikhanov <93732183+Zamdikhanov@users.noreply.github.com>
2022-03-16 19:08:37 +0400

    optimized code

--- src/components/YandexMap/YandexMap.jsx
+++ src/components/YandexMap/YandexMap.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable consistent-return */
 import { YMaps, Map, Placemark } from 'react-yandex-maps';
 import { useEffect, useRef, useState } from 'react';
@@ -12,14 +12,14 @@ function YandexMap() {
     const API_KEY = process.env.REACT_APP_YANDEX_API_KEY;
     const dispatch = useDispatch();
 
-    const { selectedCity, availablePointsInSelectedCity, selectedPoint } = useSelector(state => state.location);
+    const { selectedCity, availablePointsInSelectedCity, selectedPoint } =
+        useSelector((state) => state.location);
 
     const map = useRef(null);
 
     const [ymap, setYmap] = useState(null);
     const [zoom, setZoom] = useState(8);
     const [center, setCenter] = useState([54.314387, 48.402588]);
-
     const [coordinates, setCoordinates] = useState(null);
 
     const getCoordinates = async (address) => {
@@ -31,7 +31,11 @@ function YandexMap() {
     };
 
     const changeMapCenter = async (address, isCity = false) => {
-        if (isCity) { setZoom(10) } else { setZoom(16) };
+        if (isCity) {
+            setZoom(10);
+        } else {
+            setZoom(16);
+        }
         const coords = await getCoordinates(address);
         setCenter(coords);
     };
@@ -39,41 +43,44 @@ function YandexMap() {
     const getPoints = async (points) => {
         const newCoordinates = [];
         for (const item of points) {
-            const newCoordinate = await getCoordinates(`${selectedCity}, ${item.address}`);
+            const newCoordinate = await getCoordinates(
+                `${selectedCity}, ${item.address}`,
+            );
             newCoordinates.push({ newCoordinate, point: item });
         }
         setCoordinates(newCoordinates);
     };
 
     useEffect(() => {
-        if (!selectedPoint && selectedCity) { changeMapCenter(selectedCity, true) };
-        if (selectedPoint) { changeMapCenter(`${selectedCity}, ${selectedPoint}`) };
+        if (!selectedPoint && selectedCity) changeMapCenter(selectedCity, true);
+        if (selectedPoint) changeMapCenter(`${selectedCity}, ${selectedPoint}`);
     }, [selectedPoint, selectedCity]);
 
     useEffect(() => {
-        if (!selectedPoint && selectedCity) { changeMapCenter(selectedCity, true) };
-        if (selectedPoint) { changeMapCenter(`${selectedCity}, ${selectedPoint}`) };
+        if (!selectedPoint && selectedCity) changeMapCenter(selectedCity, true);
+        if (selectedPoint) changeMapCenter(`${selectedCity}, ${selectedPoint}`);
     }, []);
 
     useEffect(() => {
-        if (availablePointsInSelectedCity) { getPoints(availablePointsInSelectedCity) };
-        if (selectedCity) { changeMapCenter(selectedCity, true) };
+        if (availablePointsInSelectedCity)
+            getPoints(availablePointsInSelectedCity);
+        if (selectedCity) changeMapCenter(selectedCity, true);
     }, [availablePointsInSelectedCity]);
 
     const handleClick = (e, point) => {
         dispatch(setPoint(point));
         const placemarkCoords = e.get('coords');
-        if (map.current) {
-            map.current.setCenter(placemarkCoords);
-        }
+        if (map.current) map.current.setCenter(placemarkCoords);
     };
 
     return (
-        <YMaps query={{
-            apikey: API_KEY,
-            ns: 'use-load-option',
-            load: 'geocode'
-        }}>
+        <YMaps
+            query={{
+                apikey: API_KEY,
+                ns: 'use-load-option',
+                load: 'geocode',
+            }}
+        >
             <div className={css.map}>
                 <Map
                     width="100%"
@@ -82,19 +89,22 @@ function YandexMap() {
                     onLoad={(ymaps) => setYmap(ymaps)}
                     instanceRef={map}
                 >
-                    {coordinates && coordinates?.map((coordinate) => (
-                        <Placemark
-                            geometry={coordinate.newCoordinate}
-                            options={{
-                                iconLayout: 'default#image',
-                                iconImageSize: [18, 18],
-                                iconImageHref: mapMark,
-                                iconImageOffset: [-9, -9],
-                            }}
-                            key={coordinate[0]}
-                            onClick={(e) => { handleClick(e, coordinate.point.address) }}
-                        />
-                    ))}
+                    {coordinates &&
+                        coordinates?.map((coordinate) => (
+                            <Placemark
+                                geometry={coordinate.newCoordinate}
+                                options={{
+                                    iconLayout: 'default#image',
+                                    iconImageSize: [18, 18],
+                                    iconImageHref: mapMark,
+                                    iconImageOffset: [-9, -9],
+                                }}
+                                key={coordinate[0]}
+                                onClick={(e) => {
+                                    handleClick(e, coordinate.point.address);
+                                }}
+                            />
+                        ))}
                 </Map>
             </div>
         </YMaps>
